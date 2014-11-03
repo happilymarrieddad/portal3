efc844958dbbf4f5a77ce61d2d7a49606f686935
Nick Kotenberg <happilymarrieddad@yahoo.com>
2014-11-02 17:46:17 -0700

    er; asfer
    aegr

--- app/config/database.php
+++ app/config/database.php
@@ -53,7 +53,7 @@ return array(
 		),
 
 		'mysql' => array(
-			'driver'    => 'mysql',,
+			'driver'    => 'mysql',
             'host'      => getenv('DB_HOST'),
             'database'  => getenv('DB_DATA'),
             'username'  => getenv('DB_USER'),
